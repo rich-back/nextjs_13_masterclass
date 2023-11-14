575f2162f6ea3575e4c1db8cd0afcf9fc6e79afc
rich-back <backhouse.richard@gmail.com>
2023-11-14 20:36:47 +0000

    🎉 Initial commit

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,10 @@
+import CreateForm from "./CreateForm";
+
+export default function CreateTicket() {
+  return (
+    <main>
+      <h2 className="text-center text-primary">Add a new Ticket</h2>
+      <CreateForm />
+    </main>
+  );
+}
